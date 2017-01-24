bcfdb6148c334916d9d3afbf64da3965357cd957
Jan Borsodi <jborsodi@gmail.com>
2017-01-24 20:47:15 +0100

    Updated main file

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import signal
+
+from haproxy.cli import main
+
+signal.signal(signal.SIGCHLD, signal.SIG_IGN)
+
+
+if __name__ == "__main__":
+    main()
