a8b0246c6177f92863b1c4bbc937cf92e0aedbc7
Jan Borsodi <jborsodi@gmail.com>
2017-09-04 21:56:48 +0200

    Catch exceptions around service fetching

--- haproxy/manager.py
+++ haproxy/manager.py
@@ -23,38 +23,41 @@ def get_services2():
     all_ports = set()
     services = {}
 
-    for i in client.read('/services').children:
-        service_name = i.key[1:].split("/")[-1]
+    try:
+        for i in client.read('/services').children:
+            service_name = i.key[1:].split("/")[-1]
 
-        service_path = '/services/' + service_name
-        service = Service(service_name, ports=[80])
-        try:
-            config = json.loads(client.get(service_path + '/config').value)
-        except (etcd.EtcdKeyNotFound, KeyError, ValueError):
-            continue
-        if 'ports' in config and isinstance(config['ports'], list):
-            service.ports = config['ports']
-        if 'hosts' in config and isinstance(config['hosts'], list):
-            service.hosts = config['hosts']
+            service_path = '/services/' + service_name
+            service = Service(service_name, ports=[80])
+            try:
+                config = json.loads(client.get(service_path + '/config').value)
+            except (etcd.EtcdKeyNotFound, KeyError, ValueError):
+                continue
+            if 'ports' in config and isinstance(config['ports'], list):
+                service.ports = config['ports']
+            if 'hosts' in config and isinstance(config['hosts'], list):
+                service.hosts = config['hosts']
 
-        try:
-            for backend_item in client.read(service_path + '/backends').children:
-                backend_name = backend_item.key[1:].split("/")[-1]
-                backend_path = service_path + '/backends/' + backend_name
-                try:
-                    backend_config = json.loads(client.get(backend_path + '/config').value)
-                    service.backends.append(Backend(
-                        backend_name,
-                        port=backend_config['port'],
-                        host=backend_config['host'],
-                    ))
-                except (etcd.EtcdKeyNotFound, KeyError, ValueError):
-                    continue
-        except (etcd.EtcdKeyNotFound, KeyError):
-            pass
-        if service.backends:
-            services[service_name] = service
-            all_ports.update(list(service.ports))
+            try:
+                for backend_item in client.read(service_path + '/backends').children:
+                    backend_name = backend_item.key[1:].split("/")[-1]
+                    backend_path = service_path + '/backends/' + backend_name
+                    try:
+                        backend_config = json.loads(client.get(backend_path + '/config').value)
+                        service.backends.append(Backend(
+                            backend_name,
+                            port=backend_config['port'] or 80,
+                            host=backend_config['host'],
+                        ))
+                    except (etcd.EtcdKeyNotFound, KeyError, ValueError):
+                        continue
+            except (etcd.EtcdKeyNotFound, KeyError):
+                pass
+            if service.backends:
+                services[service_name] = service
+                all_ports.update(list(service.ports))
+    except (etcd.EtcdKeyNotFound, KeyError, ValueError):
+        pass
 
     return {
         'services': services,
